7d5494ebcca62d8dad607cd10c6403e48e6848a2
maizzon14 <mamami01@alumnatflorida.es>
2026-01-29 10:03:41 +0100

    Script Vector3D
    He hecho la carpeta SCRIPTS con el script Vector3D

---
+++ Assets/Scripts/Vector3D.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Vector3D : MonoBehaviour
+{
+    public float X { get; private set; }
+    public float Y { get; private set; }
+    public float z { get; private set; }
+}
